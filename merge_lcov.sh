34a18214db67a2e5820760b4a63307f12eb26b7c
jeanfortheweb <jeanfortheweb@gmail.com>
2018-01-24 00:09:31 +0100

    fixing coveralls

--- merge_lcov.sh
+++ merge_lcov.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while read FILENAME; do
     LCOV_INPUT_FILES="$LCOV_INPUT_FILES -a \"$FILENAME\""
-done < <( find $1 -maxdepth 3 -name lcov.info )
+done < <( find ./packages -maxdepth 3 -name lcov.info )
 
-eval lcov "${LCOV_INPUT_FILES}" -o $1/$2
+eval lcov "${LCOV_INPUT_FILES}" -o ./lcov.info
